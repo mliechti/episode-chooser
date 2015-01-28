16b8956d97410fc602d55fbdf4c11cedd5c3bf4d
Mark Liechti <mark.liechti@gmail.com>
2015-01-28 18:35:52 -0500

    get user input

--- src/chooser/Chooser.java
+++ src/chooser/Chooser.java
@@ -1,8 +1,34 @@
 package chooser;
 
+import java.util.Scanner;
+
 public class Chooser {
 	
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		Chooser chooser = new Chooser();
+		chooser.getSeriesFromUser();
 	}
+	
+	//Get TV series to look up from user
+	public String getSeriesFromUser() {
+		Scanner keyboard = new Scanner(System.in);
+		try {
+		
+			System.out.println("What TV series do you want to watch?");
+			
+			String tvSeries = keyboard.nextLine();
+			System.out.println("You chose " + tvSeries);
+			return tvSeries;
+		}
+		finally {
+			keyboard.close();
+		}
+		
+	}
+	//Lookup the full list of episodes from internet
+	
+	//Randomly choose one of the episodes
+	
+	//Start playing the episode (should be another module)
 }
