3368d71e4aa0b54ce711f5e763ec13b0c983a130
Mark Liechti <mark.liechti@gmail.com>
2015-01-30 21:55:35 -0500

    code to parse the csv and iterate through the episode list

--- src/chooser/Chooser.java
+++ src/chooser/Chooser.java
@@ -1,13 +1,25 @@
 package chooser;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 public class Chooser {
+	private final static int SEASON = 1;
+	private final static int EPISODE = 2;
+	private final static int NAME = 5;
 	
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		Chooser chooser = new Chooser();
-		chooser.getSeriesFromUser();
+		String tvSeries = chooser.getSeriesFromUser();
+		chooser.getRandomEpisode(tvSeries);
 	}
 	
 	//Get TV series to look up from user
@@ -26,9 +38,31 @@ public class Chooser {
 		}
 		
 	}
-	//Lookup the full list of episodes from internet
-	
-	//Randomly choose one of the episodes
+	//Lookup the full list of episodes from internet and randomly choose one
+	public String getRandomEpisode(String tvSeries) {
+		// get list from internet
+		URL csvURL;
+		try {
+			csvURL = new URL("http://epguides.com/common/exportToCSV.asp?rage=18411");
+			CSVParser parser = CSVParser.parse(csvURL, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+			for (CSVRecord csvRecord : parser) {
+				if (csvRecord.size() > 5) {
+					System.out.println(csvRecord.get(SEASON) + " - " + csvRecord.get(EPISODE) + " - " + csvRecord.get(NAME));
+				}
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Failure on csv parsing of url");
+		} 
+
+
+		return "";
+	}
+
 	
 	//Start playing the episode (should be another module)
 }
